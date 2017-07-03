eabb2338214e937f72e1bcc63116e1487dd3ad86
yuri <kamnev.u1969@gmail.com>
2017-07-04 05:19:08 +0800

    Adding constant time planner

--- trajectories.py
+++ trajectories.py
@@ -25,11 +25,73 @@ def check_profile_params(x, y, v0, a0, a_max, v_max):
     return len(x)
 
 
-def trap_plan_with_acceleration(x, y, a0, v_max, a_max):
-    pass
+def trapezoidal_planner(x, y, v0, a0, v_max, a_max):
+    """
+    x, y    --- lists containing source and destination points
+    T       --- execution time
+    v0, a0  --- lists containing initial conditions(end velocity/acceleration
+                    will be the same)
+    v_max, a_max    --- max acceleration/velocity
+
+    Returns trajectory parameters:
+        t  ---  time trajectory execution takes
+        ta ---  time of acceleration/deceleration segments
+        tc ---  constant speed segment time
+        sa ---  length of acceleration segment
+        sc ---  length of constant speed segment
+        vc ---  speed at constant speed segment(or maximum speed achieved)
+    """
+    ta = (v_max - v0)/(a_max)
+    sa = a_max*(ta**2)/2. + (v0*ta)
+
+    if y - x < 2*sa:
+        raise PlanningError("Failed to plan trapezoidal profile")
+
+    t = (y - x - a_max*(ta**2) - 2.*v0*ta)/v_max + 2.*ta
+    tc = t - 2.*ta
+    sc = tc*v_max+0.0
+    vc = v_max
+
+    return (t, ta, tc, sa, sc, vc)
 
 
-def trap_plan_with_time(x, y, v0, a0, v_max, a_max):
+def minimum_time_planner(x, y, v0, a0, v_max, a_max):
+    """
+    x, y    --- lists containing source and destination points
+    T       --- execution time
+    v0, a0  --- lists containing initial conditions(end velocity/acceleration
+                    will be the same)
+    v_max, a_max    --- max acceleration/velocity
+
+    Returns trajectory parameters:
+        t  ---  time trajectory execution takes
+        ta ---  time of acceleration/deceleration segments
+        tc ---  constant speed segment time
+        sa ---  length of acceleration segment
+        sc ---  length of constant speed segment
+        vc ---  speed at constant speed segment(or maximum speed achieved)
+    """
+    a = a_max
+    D = np.sqrt((v0**2) + (y-x)*a_max)
+    denom = (a_max/2.)
+
+    t1 = (-v0 + D)/denom
+    t2 = (-v0 - D)/denom
+
+    t = max(t1, t2)
+    ta = 0.5*t
+    tc = 0
+    sa = v0*ta + a*(ta**2)/2.
+    sc = 0
+
+    _v_max = v0 + a*ta
+    if _v_max > v_max:
+        raise PlanningError("Maximum speed exceeds bound")
+
+    return (t, ta, tc, sa, sc, _v_max)
+
+
+def constant_time_planner(x, y, v0, a0, v_max, a_max, t):
     pass
 
 
@@ -40,6 +102,10 @@ def trapezoidal_profile(x, y, v0, a0, v_max, a_max):
     v0, a0  --- lists containing initial conditions(end velocity/acceleration
                     will be the same)
     v_max, a_max    --- max acceleration/velocity
+
+    Returns number of DOF, list of times each DOF trajectory takes to be
+        executed and function of time which returns desired acceleration,
+        speed and position at time t.
     """
     dof = check_profile_params(x, y, v0, a0, v_max, a_max)
 
@@ -48,29 +114,28 @@ def trapezoidal_profile(x, y, v0, a0, v_max, a_max):
     TC = []
     SA = []
     SC = []
+    VC = []
+
+    trajectory_params = [T, TA, TC, SA, SC, VC]
 
     for i in range(dof):
-        ta = (v_max[i] - v0[i] + 0.0)/(a_max[i] + 0.0)
-        sa = a_max[i]*(ta**2)/2. + (v0[i]*ta)
+        try:
+            params = minimum_time_planner(x[i], y[i], v0[i],
+                                          a0[i], v_max[i], a_max[i])
+        except PlanningError:
+            print("Failed to plan minimum time profile")
+            params = trapezoidal_planner(x[i], y[i], v0[i],
+                                         a0[i], v_max[i], a_max[i])
 
-        if y[i] - x[i] < 2*sa:
-            raise PlanningError("Failed to plan trapezoidal profile")
-
-        t = (y[i] - x[i] - a_max[i]*(ta**2) - 2.*v0[i]*ta)/v_max[i] + 2.*ta
-        tc = t - 2.*ta
-        sc = tc*v_max[i]+0.0
-
-        T.append(t)
-        TA.append(ta)
-        TC.append(tc)
-        SA.append(sa)
-        SC.append(sc)
+        for p_list, p in zip(trajectory_params, params):
+            p_list.append(p)
 
     print("T: " + str(T))
     print("TA: " + str(TA))
     print("TC: " + str(TC))
     print("SA: " + str(SA))
     print("SC: " + str(SC))
+    print("VC: " + str(VC))
 
     def trajectory(_t):
         """
@@ -81,7 +146,7 @@ def trapezoidal_profile(x, y, v0, a0, v_max, a_max):
 
         point = np.zeros((dof, 3))
 
-        for i, t, ta, tc, sa, sc in zip(range(dof), T, TA, TC, SA, SC):
+        for i, t, ta, tc, sa, sc, vc in zip(range(dof), T, TA, TC, SA, SC, VC):
             if _t == 0:
                 point[i][ACCELERATION_ID] = a0[i]
                 point[i][SPEED_ID] = v0[i]
@@ -94,15 +159,15 @@ def trapezoidal_profile(x, y, v0, a0, v_max, a_max):
             elif ta <= _t < ta+tc:
                 point[i][ACCELERATION_ID] = 0
                 point[i][SPEED_ID] = v_max[i]
-                point[i][POSITION_ID] = x[i] + sa + v_max[i]*(_t - ta)
+                point[i][POSITION_ID] = x[i] + sa + vc*(_t - ta)
 
             elif ta+tc <= _t < t:
                 point[i][ACCELERATION_ID] = -a_max[i]
-                point[i][SPEED_ID] = v_max[i] - a_max[i]*(_t - ta - tc)
+                point[i][SPEED_ID] = vc - a_max[i]*(_t - ta - tc)
                 point[i][POSITION_ID] = x[i] + sa+sc\
-                    - a_max[i]*((_t-ta-tc)**2)/2 + v_max[i]*(_t-ta-tc)
+                    - a_max[i]*((_t-ta-tc)**2)/2 + vc*(_t-ta-tc)
 
-            elif _t == t:
+            elif _t >= t:
                 point[i][ACCELERATION_ID] = a0[i]
                 point[i][SPEED_ID] = v0[i]
                 point[i][POSITION_ID] = y[i]
@@ -116,10 +181,10 @@ def trapezoidal_profile(x, y, v0, a0, v_max, a_max):
 
 
 def plot_trajectory(traj, dt):
-    timesteps = traj.time[0] / dt
     dof = traj.dof
+    timesteps = max(traj.time) / dt
+    time = np.linspace(0, max(traj.time), timesteps)
 
-    time = np.linspace(0, traj.time[0], timesteps)
     # NOW
     # profiles[t]           --- profiles for each DOF at time x[t]
     # profiles[t][d]        --- profile for d DOF at time x[t]
@@ -136,24 +201,30 @@ def plot_trajectory(traj, dt):
         for p in range(3):
             r_profiles[d, p, :] = profiles[:, d, p]
 
-    for i, profile in zip(range(dof), r_profiles):
-        fig = plt.figure(i)
-        fig.suptitle("DOF %d profiles" % i)
+    fig = plt.figure(0)
+    fig.suptitle("DOF profiles")
 
-        plt.subplot(311)
+    for i, profile in zip(range(dof), r_profiles):
+        plt.subplot(300 + dof*10 + (i+1))
         plt.title("Acceleration profile")
         plt.plot(time, profile[ACCELERATION_ID][:])
+        plt.xlim()
+        plt.ylim()
 
-        plt.subplot(312)
+        plt.subplot(300 + dof*10 + (i+1)+dof)
         plt.title("Speed profile")
         plt.plot(time, profile[SPEED_ID][:])
+        plt.xlim()
+        plt.ylim()
 
-        plt.subplot(313)
+        plt.subplot(300 + dof*10 + (i+1)+dof*2)
         plt.title("Position profile")
         plt.plot(time, profile[POSITION_ID][:])
+        plt.xlim()
+        plt.ylim()
 
-        plt.tight_layout()
-        plt.show()
+    plt.tight_layout()
+    plt.show()
 
 
 class Trajectory(object):
@@ -204,12 +275,12 @@ class Trajectory(object):
 
 
 if __name__ == "__main__":
-    src = [1+0.0]
-    dst = [6+0.0]
-    a0 = [1+0.0]
-    v0 = [1+0.0]
-    a_max = [2+0.0]
-    v_max = [3+0.0]
+    src = [1+0.0, 0.0]
+    dst = [2+0.0, 15.0]
+    a0 = [0+0.0, 0.]
+    v0 = [0+0.0, 0.]
+    a_max = [2+0.0, 3.]
+    v_max = [3+0.0, 5.]
 
     try:
         trajectory = Trajectory.plan_trajectory(src, dst, v0, a0, v_max, a_max)
