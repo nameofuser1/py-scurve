c4855e981e749e614043f2689b61320610b89007
kripton <kamnev.u1969@gmail.com>
2017-09-13 13:24:12 +0300

    Fixed bug with logging

--- pyscurve/__init__.py
+++ pyscurve/__init__.py
@@ -3,32 +3,40 @@ import os
 from scurve import ScurvePlanner
 from trajectory import Trajectory, plot_trajectory
 
-cur_dir = os.path.dirname(os.path.abspath(__file__))
 
-FNAME_EXT = ".log"
-PLANNING_LOG_FNAME = cur_dir + os.path.sep + "planning" + FNAME_EXT
-TRAJECTORY_LOG_FNAME = cur_dir + os.path.sep + "trajectory" + FNAME_EXT
-
-planning_logger = logging.getLogger("planning_logger")
-planning_logger.setLevel(logging.DEBUG)
+DEBUG = False
 
 trajectory_logger = logging.getLogger("trajectory_logger")
-trajectory_logger.setLevel(logging.DEBUG)
+planning_logger = logging.getLogger("planning_logger")
 
-plan_fhandler = logging.FileHandler(PLANNING_LOG_FNAME, 'w')
-plan_fhandler.setLevel(logging.DEBUG)
 
-traj_fhandler = logging.FileHandler(TRAJECTORY_LOG_FNAME, 'w')
-traj_fhandler.setLevel(logging.DEBUG)
+if DEBUG:
+    cur_dir = os.path.dirname(os.path.abspath(__file__))
 
-planning_formatter = logging.Formatter("%(asctime)s - %(levelname)s - "
-                                       "LINE %(lineno)s "
-                                       "-\r\n%(message)s")
-trajectory_formatter = logging.Formatter("%(message)s")
+    FNAME_EXT = ".log"
+    PLANNING_LOG_FNAME = cur_dir + os.path.sep + "planning" + FNAME_EXT
+    TRAJECTORY_LOG_FNAME = cur_dir + os.path.sep + "trajectory" + FNAME_EXT
 
-plan_fhandler.setFormatter(planning_formatter)
-traj_fhandler.setFormatter(trajectory_formatter)
+    planning_logger.setLevel(logging.DEBUG)
+    trajectory_logger.setLevel(logging.DEBUG)
 
-planning_logger.addHandler(plan_fhandler)
-trajectory_logger.addHandler(traj_fhandler)
+    plan_fhandler = logging.FileHandler(PLANNING_LOG_FNAME, 'w')
+    plan_fhandler.setLevel(logging.DEBUG)
 
+    traj_fhandler = logging.FileHandler(TRAJECTORY_LOG_FNAME, 'w')
+    traj_fhandler.setLevel(logging.DEBUG)
+
+    planning_formatter = logging.Formatter("%(asctime)s - %(levelname)s - "
+                                        "LINE %(lineno)s "
+                                        "-\r\n%(message)s")
+    trajectory_formatter = logging.Formatter("%(message)s")
+
+    plan_fhandler.setFormatter(planning_formatter)
+    traj_fhandler.setFormatter(trajectory_formatter)
+
+    planning_logger.addHandler(plan_fhandler)
+    trajectory_logger.addHandler(traj_fhandler)
+
+else:
+    planning_logger.setLevel(logging.FATAL)
+    trajectory_logger.setLevel(logging.FATAL)
