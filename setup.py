001e8f76ade8bfb911b93583e80d84f3f1388666
Ibrahim <ibrahim78786@gmail.com>
2022-10-27 10:46:57 -0500

    Bump patch version number to 1.0.2;
    This will be added as a dependency to the multirotor library.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyscurve',
-    version='1.0.1',
+    version='1.0.2',
     packages=find_packages(),
     install_requires=['numpy', 'matplotlib']
 )
